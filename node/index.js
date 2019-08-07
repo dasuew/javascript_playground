680dbf45f714d0b12d69b8cc1709d92bec975607
Uwe Manteuffel <dasuew@hotmail.de>
2019-08-07 11:33:49 +0200

    node example stuff

--- node/index.js
+++ node/index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.listen(3000, () => console.log('listening to 3000'))
+app.listen(3000, () => console.log('listening to port 3000'))
 app.use(express.static('public'))
+app.use(express.json({ limit: '1mb' }))
 
+app.post('/api', (request, response) => {
+    const data = request.body
+
+    response.json({
+        status: 'success',
+        latitude: data.lat,
+        longitude: data.long
+    })
+})
