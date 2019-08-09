da99ba485d91f988724a7ba1a00d849a16bb95f4
Uwe Manteuffel <dasuew@hotmail.de>
2019-08-09 14:57:30 +0200

    switch to auto port

--- weather_app/index.js
+++ weather_app/index.js
@@ -4,8 +4,9 @@ const fetch = require('node-fetch')
 require('dotenv').config()
 
 const app = express()
+const port = process.env.PORT || 3000
 
-app.listen(3000, () => console.log('listening to port 3000'))
+app.listen(port, () => console.log(`Listening to port ${port}`))
 app.use(express.static('public'))
 app.use(express.json({ limit: '1mb' }))
 
